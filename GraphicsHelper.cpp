5a4d9fd0d7f759d6dbfd77e850d3b56f08cf622e
i.ilyasov <igorilyasov94@gmail.com>
2020-12-11 04:23:37 +0300

    Desc initializer funcs for helper

--- GraphicsHelper.cpp
+++ GraphicsHelper.cpp
@@ -58,3 +58,65 @@ void createGraphicsPipelineState(ID3D12Device*& device, D3D12_INPUT_LAYOUT_DESC&
 
 	ThrowIfFailed(device->CreateGraphicsPipelineState(&pipelineStateDesc, IID_PPV_ARGS(&pipelineState)));
 }
+
+void setupRasterizerDesc(D3D12_RASTERIZER_DESC& rasterizerDesc, D3D12_CULL_MODE cullMode = D3D12_CULL_MODE_NONE)
+{
+	rasterizerDesc.AntialiasedLineEnable = false;
+	rasterizerDesc.ConservativeRaster = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+	rasterizerDesc.CullMode = cullMode;
+	rasterizerDesc.DepthBias = D3D12_DEFAULT_DEPTH_BIAS;
+	rasterizerDesc.DepthBiasClamp = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;
+	rasterizerDesc.DepthClipEnable = true;
+	rasterizerDesc.FillMode = D3D12_FILL_MODE_SOLID;
+	rasterizerDesc.ForcedSampleCount = 0;
+	rasterizerDesc.FrontCounterClockwise = false;
+	rasterizerDesc.MultisampleEnable = false;
+	rasterizerDesc.SlopeScaledDepthBias = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;
+}
+
+void setupBlendDesc(D3D12_BLEND_DESC& blendDesc, bool blendOn = false, 
+	D3D12_BLEND srcBlend = D3D12_BLEND_ONE, D3D12_BLEND destBlend = D3D12_BLEND_ZERO, D3D12_BLEND_OP blendOp = D3D12_BLEND_OP_ADD,
+	D3D12_BLEND srcBlendAlpha = D3D12_BLEND_ONE, D3D12_BLEND destBlendAlpha = D3D12_BLEND_ZERO, D3D12_BLEND_OP blendOpAlpha = D3D12_BLEND_OP_ADD)
+{
+	blendDesc.AlphaToCoverageEnable = false;
+	blendDesc.IndependentBlendEnable = false;
+	
+	const D3D12_RENDER_TARGET_BLEND_DESC rtBlendDescDefault =
+	{
+		false, false,
+		D3D12_BLEND_ONE, D3D12_BLEND_ZERO, D3D12_BLEND_OP_ADD,
+		D3D12_BLEND_ONE, D3D12_BLEND_ZERO, D3D12_BLEND_OP_ADD,
+		D3D12_LOGIC_OP_NOOP, D3D12_COLOR_WRITE_ENABLE_ALL
+	};
+
+	for (auto renderTargetId = 0; renderTargetId < 8; renderTargetId++)
+	{
+		blendDesc.RenderTarget[renderTargetId] = rtBlendDescDefault;
+	}
+
+	blendDesc.RenderTarget[0].BlendEnable = blendOn;
+	blendDesc.RenderTarget[0].SrcBlend = srcBlend;
+	blendDesc.RenderTarget[0].DestBlend = destBlend;
+	blendDesc.RenderTarget[0].BlendOp = blendOp;
+	blendDesc.RenderTarget[0].SrcBlendAlpha = srcBlendAlpha;
+	blendDesc.RenderTarget[0].DestBlendAlpha = destBlendAlpha;
+	blendDesc.RenderTarget[0].BlendOpAlpha = blendOpAlpha;
+}
+
+void setupDepthStencilDesc(D3D12_DEPTH_STENCIL_DESC& depthStencilDesc, bool depthEnable)
+{
+	depthStencilDesc.DepthEnable = depthEnable;
+	depthStencilDesc.DepthFunc = D3D12_COMPARISON_FUNC_LESS;
+	depthStencilDesc.DepthWriteMask = D3D12_DEPTH_WRITE_MASK_ALL;
+	depthStencilDesc.StencilEnable = false;
+	depthStencilDesc.StencilReadMask = D3D12_DEFAULT_STENCIL_READ_MASK;
+	depthStencilDesc.StencilWriteMask = D3D12_DEFAULT_STENCIL_WRITE_MASK;
+	
+	const D3D12_DEPTH_STENCILOP_DESC depthStencilOpDesc =
+	{
+		D3D12_STENCIL_OP_KEEP, D3D12_STENCIL_OP_KEEP, D3D12_STENCIL_OP_KEEP, D3D12_COMPARISON_FUNC_ALWAYS
+	};
+
+	depthStencilDesc.BackFace = depthStencilOpDesc;
+	depthStencilDesc.FrontFace = depthStencilOpDesc;
+}
--- GraphicsHelper.h
+++ GraphicsHelper.h
@@ -12,3 +12,10 @@ void createGraphicsPipelineState(ID3D12Device*& device, D3D12_INPUT_LAYOUT_DESC&
 	D3D12_RASTERIZER_DESC& rasterizerDesc, D3D12_BLEND_DESC& blendDesc, D3D12_DEPTH_STENCIL_DESC& depthStencilDesc,
 	DXGI_FORMAT rtvFormat, ID3DBlob*& vertexShader, ID3DBlob*& pixelShader, ID3D12PipelineState*& pipelineState);
 
+void setupRasterizerDesc(D3D12_RASTERIZER_DESC& rasterizerDesc, D3D12_CULL_MODE cullMode = D3D12_CULL_MODE_NONE);
+
+void setupBlendDesc(D3D12_BLEND_DESC& blendDesc, bool blendOn = false,
+	D3D12_BLEND srcBlend = D3D12_BLEND_ONE, D3D12_BLEND destBlend = D3D12_BLEND_ZERO, D3D12_BLEND_OP blendOp = D3D12_BLEND_OP_ADD,
+	D3D12_BLEND srcBlendAlpha = D3D12_BLEND_ONE, D3D12_BLEND destBlendAlpha = D3D12_BLEND_ZERO, D3D12_BLEND_OP blendOpAlpha = D3D12_BLEND_OP_ADD);
+
+void setupDepthStencilDesc(D3D12_DEPTH_STENCIL_DESC& depthStencilDesc, bool depthEnable);
