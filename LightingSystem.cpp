4c76cc1c8cfe6d6c341a83f8b9d0afd579ea8efa
i.ilyasov <igorilyasov94@gmail.com>
2021-05-31 01:50:48 +0300

    Light system in process

--- LightingSystem.cpp
+++ LightingSystem.cpp
@@ -7,6 +7,30 @@ Graphics::LightingSystem& Graphics::LightingSystem::GetInstance()
 	return instance;
 }
 
+Graphics::PointLightId Graphics::LightingSystem::CreatePointLight(float3 position, float3 color, float radius, float intensity, bool isShadowCaster)
+{
+	pointLights.push_back(PointLight(position, color, radius, intensity, isShadowCaster));
+
+	return PointLightId(pointLights.size() - 1);
+}
+
+Graphics::PointLightId Graphics::LightingSystem::CreatePointLight(PointLight&& pointLight)
+{
+	pointLights.push_back(std::forward<PointLight>(pointLight));
+
+	return PointLightId(pointLights.size() - 1);
+}
+
+Graphics::PointLight& Graphics::LightingSystem::SetPointLight(const PointLightId& pointLightId)
+{
+	return pointLights[pointLightId.value];
+}
+
+void Graphics::LightingSystem::Clear()
+{
+	pointLights.clear();
+}
+
 Graphics::LightingSystem::LightingSystem()
 {
 
--- LightingSystem.h
+++ LightingSystem.h
@@ -6,12 +6,43 @@
 
 namespace Graphics
 {
+	template<uint8_t Category>
+	class LightId
+	{
+	public:
+		LightId()
+			: value(0), category(Category)
+		{
+
+		}
+
+		LightId(uint32_t resourceId)
+			: value(resourceId), category(Category)
+		{
+
+		}
+
+		uint32_t value;
+
+	private:
+		friend class LightingSystem;
+
+		uint8_t category;
+	};
+
+	using PointLightId = typename LightId<0>;
+
 	class LightingSystem
 	{
 	public:
 		static LightingSystem& GetInstance();
 
+		PointLightId CreatePointLight(float3 position, float3 color, float radius, float intensity, bool isShadowCaster);
+		PointLightId CreatePointLight(PointLight&& pointLight);
 
+		PointLight& SetPointLight(const PointLightId& pointLightId);
+		
+		void Clear();
 
 	private:
 		LightingSystem();
